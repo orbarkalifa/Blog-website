e9edf7f4a096c9df7070bc9b2b750d470dce820e
Or Bar Califa <orbarkalifa@gmail.com>
2023-03-08 22:05:59 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const app = express();
 
@@ -12,10 +13,60 @@ const homeStartingContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing 
 const aboutContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut faucibus pulvinar elementum integer enim neque volutpat. Nullam ac tortor vitae purus faucibus ornare suspendisse. Nec tincidunt praesent semper feugiat nibh sed pulvinar. Ut consequat semper viverra nam libero justo laoreet sit amet. Nunc faucibus a pellentesque sit amet. Ac auctor augue mauris augue neque gravida in fermentum. Pharetra massa massa ultricies mi quis.';
 const contactContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean et tortor at risus viverra adipiscing at in. Nisl nunc mi ipsum faucibus vitae. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est. Elit sed vulputate mi sit amet.';
 
+const posts = [];
+
 app.get("/", (req,res)=>{
-    app.render('home');
+    res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts
+    });
 });
 
+app.get("/posts/:postName", (req,res)=>{
+
+    posts.forEach(post=>{
+        const requestedTitle = _.lowerCase(req.params.postName);
+        const postTitle = _.lowerCase(post.title)
+
+        if (requestedTitle === postTitle) {
+            res.render("post", {
+                postTitle: post.title,
+                postContent: post.content
+            });
+        } else {
+            res.render("post", {
+                postTitle: "ERROR 404",
+                postContent: "The page you are looking for was not found"
+            });
+        }
+
+    });
+});
+
+
+
+app.get("/about", (req,res)=>{
+    res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", (req,res)=>{
+    res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", (req,res)=>{
+    res.render("compose");
+});
+
+app.post("/compose", (req,res)=>{
+
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postContent
+    };
+    
+    posts.push(post);
+    res.redirect('/');
+});
 
 
 
